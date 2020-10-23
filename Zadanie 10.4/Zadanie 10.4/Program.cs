5ba81bf490a213d3696a93714f48619783fff5e9
wafelow <71896857+wafelow@users.noreply.github.com>
2020-10-23 09:36:47 +0200

    test

---
+++ Zadanie 10.4/Zadanie 10.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tablicepraktyczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] magazyny = new string[5,2] ;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    magazyny[i, j] = Console.ReadLine();
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(magazyny[i, j] + ' ');
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
